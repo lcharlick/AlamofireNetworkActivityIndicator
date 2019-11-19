e4ee904fc947d7069e599830c6f114d6ec042118
Lachlan Charlick <lachlan.charlick@gmail.com>
2019-11-19 19:56:52 +1030

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "AlamofireNetworkActivityIndicator", targets: ["AlamofireNetworkActivityIndicator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3")
     ],
     targets: [
         .target(
